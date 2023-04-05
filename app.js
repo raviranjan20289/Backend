61bfb4e6d2c50d673ff7cc7ad79b0855115930d7
diler20289@gmail.com <diler20289@gmail.com>
2023-04-05 20:45:15 +0530

    adding products and displaying on console

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const http = require("http");
-
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next)=> {
-    console.log("I am inside middleware");
-    next();
+app.get('/add-product', (req, res) => {
+  res.send(`
+    <form method="POST" action="/add-product">
+      <label for="product-name">Product Name</label>
+      <input type="text" id="product-name" name="product-name">
+
+      <label for="product-size">Product Size</label>
+      <input type="text" id="product-size" name="product-size">
+
+      <button type="submit">Add Product</button>
+    </form>
+  `);
 });
 
-app.use((req,res, next) =>{
-    console.log("I am inside another middleware");
-    res.send("hello from express.js");
+app.post('/add-product', (req, res) => {
+  const productName = req.body['product-name'];
+  const productSize = req.body['product-size'];
+  console.log(`Product Name: ${productName}, Product Size: ${productSize}`);
+  res.send('Product added successfully!');
 });
 
-
-const server = http.createServer(app);
-
-server.listen(4000, ()=>{
-    console.log("server is connected ");
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
