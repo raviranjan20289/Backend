84fd1916d73b3df3281dd82966dc76be4293341f
diler20289@gmail.com <diler20289@gmail.com>
2023-04-05 23:35:26 +0530

    adding filter routes

---
+++ Routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+router.get('/add-product', (req, res)=>{
+    
+    res.send('<form action = "/admin/product" method = "POST" ><input type= "text" name = "title"><button type= "submit">Add Button</button></form>');
+   
+});
+
+router.post('/product', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/shop');
+});
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/add-product', (req, res) => {
-  res.send(`
-    <form method="POST" action="/add-product">
-      <label for="product-name">Product Name</label>
-      <input type="text" id="product-name" name="product-name">
+const adminRouter= require("./Routes/admin");
+const shopRouter = require("./Routes/shop");
+app.use(bodyParser.urlencoded({extended: true}));
 
-      <label for="product-size">Product Size</label>
-      <input type="text" id="product-size" name="product-size">
+app.use('/admin', adminRouter);
+app.use( '/shop', shopRouter);
 
-      <button type="submit">Add Product</button>
-    </form>
-  `);
-});
+app.use((req, res, next)=>{
+    res.status(404).send('<p>Page not found<p>');
+})
 
-app.post('/add-product', (req, res) => {
-  const productName = req.body['product-name'];
-  const productSize = req.body['product-size'];
-  console.log(`Product Name: ${productName}, Product Size: ${productSize}`);
-  res.send('Product added successfully!');
-});
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(4000, (req, res)=>{
+    console.log("server is connected ");
+})
